5671644ee7af827c4a3bc8e93a520f492a29b240
Tony Kim <kimd24@tcnj.edu>
2019-04-28 00:00:58 -0400

    try heroku

---
+++ db/migrate/20190419204848_create_legislators.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class CreateLegislators < ActiveRecord::Migration[5.2]
+  def change
+    create_table :legislators do |t|
+      t.string :name
+      t.string :title
+      t.float :total_receipts
+      t.string :cycle
+      t.string :state
+      t.string :party
+      t.float :spent
+      t.float :cash_on_hand
+      t.float :debt
+      t.string :sourceOpenSecrets
+      t.json :candContributors
+      t.json :candIndustries
+      t.json :positions
+      t.string :openSecretsID
+      t.date :birthday
+      t.string :gender
+      t.string :contact_form
+      t.string :address
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
